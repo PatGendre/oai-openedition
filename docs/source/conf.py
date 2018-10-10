cd204372bdeda196746b17dff642eb884bc91591
Jean-François Rivière <jean-francois.riviere@openedition.org>
2018-10-10 22:29:52 +0200

    conf.py : html_style default

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,7 +75,6 @@ pygments_style = None
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
